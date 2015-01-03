6c75182d32d20d1785afd748ad0b6ae95e236000
avezra <aviadezra@hotmail.com>
2015-01-04 00:43:49 +0200

    missing file

---
+++ TheGalery.Core/UserCredentials.cs
@@ -0,0 +1,22 @@
+﻿namespace TheGalery.Core
+{
+    public class UserCredentials
+    {
+        public static UserCredentials Default = new UserCredentials(CredentialsType.Deafult);
+
+        public CredentialsType CredentialsType { get; private set; }
+
+        public UserCredentials(CredentialsType credentialsType)
+        {
+            CredentialsType = credentialsType;
+        }
+    }
+
+    public enum CredentialsType
+    {
+        Deafult,
+        DropBox,
+        GoogleDrive,
+        OneDrive,
+    }
+}
